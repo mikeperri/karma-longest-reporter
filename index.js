b0f3e065806bdb45c9538ad63a9708b675d4326b
Antoine Ceol <antoine.ceol@gmail.com>
2019-10-15 23:49:00 +0200

    :sparkles: add minimum milliseconds prop

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 var os = require('os');
 
-var LongestReporter = function (baseReporterDecorator, helper, longestSpecsToReport) {
+var LongestReporter = function (baseReporterDecorator, helper, props) {
     baseReporterDecorator(this);
-
+    var maximumDisplay = props.maximumDisplay || 10;
+    var minimumMillis = props.minimumMillis || 0;
     var write = process.stdout.write.bind(process.stdout);
-    var longestSpecsToReport = longestSpecsToReport ? longestSpecsToReport : 10;
     var specs = [];
 
     this.onSpecComplete = function (browser, result) {
         var name = result.suite.join(' ') + ' ' + result.description;
-        var time = helper.formatTimeInterval(result.time);
+        var time = result.time;
 
         specs.push({
             name: name,
             time: time
         });
-    }
+    };
 
     this.onBrowserComplete = function (browser) {
         var longestSpecs = specs
             .sort(function (a, b) {
                 return a.time < b.time ? 1 : -1;
             })
-            .slice(0, longestSpecsToReport);
-
-        longestSpecs.forEach(function (spec) {
-            write(spec.time + ': ' + spec.name + os.EOL);
-        });
-    }
-}
+            .slice(0, maximumDisplay);
+        
+            longestSpecs
+            .filter(function (spec) {
+                return spec.time > minimumMillis;
+            })
+            .forEach(function (spec) {
+                write(helper.formatTimeInterval(spec.time) + ': ' + spec.name + os.EOL);
+            });
+    };
+};
 
 LongestReporter.$inject = [ 'baseReporterDecorator', 'helper', 'config.longestSpecsToReport' ];
 
